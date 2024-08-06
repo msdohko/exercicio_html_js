d561c57a1b4e1200df397b16931ba7242d620762
Eduardo <msdohko@gmail.com>
2024-08-06 02:58:11 -0300

    corrigido o limpar campo

--- main.js
+++ main.js
@@ -1,29 +1,25 @@
 const form = document.getElementById('formCalculo');
+const numeroA = document.getElementById('numeroA');
+const numeroB = document.getElementById('numeroB');
+const mensagem = document.getElementById('mensagem');
 
 form.addEventListener('submit', function(e) {
     e.preventDefault();
 
-const numeroA = document.getElementById('numeroA').value;
-const numeroB = document.getElementById('numeroB').value;
-const mensagem = document.getElementById('mensagem');
+const valorNumeroA = Number(numeroA.value);
+const valorNumeroB = Number(numeroB.value);
 
-if (numeroB > numeroA) {
-    mensagem.textContent = `Está VALIDO, campo B: ${numeroB} é maior que campo A: ${numeroA}.`;
+if (valorNumeroB > valorNumeroA) {
+    mensagem.textContent = `Está VALIDO, campo B: ${valorNumeroB} é maior que campo A: ${valorNumeroA}.`;
     mensagem.style.color = 'green';
-} else if (numeroB === numeroA) {
+} else if (valorNumeroB === valorNumeroA) {
     mensagem.textContent = 'Para comparação os números não poder ser iguais.';
     mensagem.style.color = 'black';
 } else {
-    mensagem.textContent = `Está INVALIDO, campo A: ${numeroA} é maior que campo B: ${numeroB}.`;
+    mensagem.textContent = `Está INVALIDO, campo A: ${valorNumeroA} é maior que campo B: ${valorNumeroB}.`;
     mensagem.style.color = 'red';
 } 
 
+document.getElementById('numeroA').value='';
+document.getElementById('numeroB').value='';
 });
-
-numeroA.addEventListener('focus', function()) {
-    numeroA.value='';
-}
-
-numeroB.addEventListener('focus', function()) {
-    numeroB.value='';
-}
