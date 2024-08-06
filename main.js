8369a35f60e3d13ae02f17fe0995d00d9f8ae8bd
Eduardo <msdohko@gmail.com>
2024-08-06 02:37:52 -0300

    corrigindo .js

--- main.js
+++ main.js
@@ -8,20 +8,22 @@ const numeroB = document.getElementById('numeroB').value;
 const mensagem = document.getElementById('mensagem');
 
 if (numeroB > numeroA) {
-    mensagem.textContent = `Número no campo B é ${numeroB}, sendo assim maior que no campo A que é ${numeroA}. Comparação Válida`;
+    mensagem.textContent = `Está VALIDO, campo B: ${numeroB} é maior que campo A: ${numeroA}.`;
     mensagem.style.color = 'green';
-    numeroA.value = '';
-    numeroB.value = '';
 } else if (numeroB === numeroA) {
     mensagem.textContent = 'Para comparação os números não poder ser iguais.';
     mensagem.style.color = 'black';
-    numeroA.value = '';
-    numeroB.value = '';
 } else {
-    mensagem.textContent = `Número no campo A é ${numeroA}, sendo assim maior que no campo B que é ${numeroB}. Comparação Inválida.`;
+    mensagem.textContent = `Está INVALIDO, campo A: ${numeroA} é maior que campo B: ${numeroB}.`;
     mensagem.style.color = 'red';
-    numeroA.value = '';
-    numeroB.value = '';
 } 
 
-})
+});
+
+numeroA.addEventListener('focus', function()) {
+    numeroA.value='';
+}
+
+numeroB.addEventListener('focus', function()) {
+    numeroB.value='';
+}
